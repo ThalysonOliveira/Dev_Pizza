8d864ccb429fa85000ef8e54af4ea9a1ce440228
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-22 16:26:43 -0300

    feat: create CreateUser factory

---
+++ src/factories/users/create-user.ts
@@ -0,0 +1,19 @@
+import { CreateUserController } from '../../controllers/users';
+import { CreateUserService } from '../../data/services/users';
+import { CreateUserPrisma } from '../../infra/database/prisma/repositories/users/create-user-prisma';
+import { FindUserByEmailPrisma } from '../../infra/database/prisma/repositories/users/find-user-by-email-prisma';
+import { EncrypterAdapter } from '../../infra/protocols/encrypter/encrypter-adapter';
+
+const makeCreateUser = () => {
+  const encrypter = new EncrypterAdapter(8);
+  const createUserRepository = new CreateUserPrisma();
+  const findUserbyEmail = new FindUserByEmailPrisma();
+  const createUserService = new CreateUserService(
+    findUserbyEmail,
+    createUserRepository,
+    encrypter
+  );
+  return new CreateUserController(createUserService);
+};
+
+export { makeCreateUser };
