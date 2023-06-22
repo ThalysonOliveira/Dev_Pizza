0caa35332d8147a5d2e6acd33bbd964f4cd0ee37
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-22 16:25:44 -0300

    feat: create createUserService implementation

---
+++ src/data/services/users/create-user.ts
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+import {
+  CreateUser,
+  UserData,
+} from '../../../domain/useCases/users/create-user';
+import { Encrypter } from '../../protocols/encrypter';
+import { CreateUserRepository } from '../../repositories/users/create-user';
+import { FindUserByEmailRepository } from '../../repositories/users/find-user-by-email';
+
+class CreateUserService implements CreateUser {
+  constructor(
+    private findUserByEmailRepository: FindUserByEmailRepository,
+    private createUserRepository: CreateUserRepository,
+    private encrypter: Encrypter
+  ) {}
+  async execute(input: UserData): Promise<void> {
+    const emailAlreadyExist = await this.findUserByEmailRepository.execute(
+      input.email
+    );
+
+    if (emailAlreadyExist) throw new Error('Email already exists.');
+
+    const hashedPassword = this.encrypter.encrypt(input.password);
+
+    Object.assign(input, { password: hashedPassword });
+
+    await this.createUserRepository.execute(input);
+  }
+}
+
+export { CreateUserService };
