5ee56270c0c74975f5aed638ae989944a510de4e
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-22 16:26:25 -0300

    feat: create CreateUserController

---
+++ src/controllers/users/create-user.ts
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import { CreateUser, UserData } from '../../domain/useCases/users/create-user';
+
+class CreateUserController {
+  constructor(private createUser: CreateUser) {}
+
+  async handle(input: UserData): Promise<string> {
+    try {
+      await this.createUser.execute(input);
+    } catch (error) {
+      return error as string;
+    }
+
+    return 'User created with success!';
+  }
+}
+
+export { CreateUserController };
