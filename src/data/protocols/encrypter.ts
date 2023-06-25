7dee152370ba7e4853f4d269cd979c8673d32a5d
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-24 22:18:07 -0300

    feat: create interface and encrypter implementation

--- src/data/protocols/encrypter.ts
+++ src/data/protocols/encrypter.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 interface Encrypter {
   encrypt: (value: string) => string;
+  compareEncrypt: (compareValue: string, hashValue: string) => boolean;
 }
 
 export { Encrypter };
--- src/infra/protocols/encrypter/encrypter-adapter.ts
+++ src/infra/protocols/encrypter/encrypter-adapter.ts
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 import { Encrypter } from '../../../data/protocols/encrypter';
-import { hashSync } from 'bcrypt';
+import { compareSync, hashSync } from 'bcrypt';
 
 class EncrypterAdapter implements Encrypter {
   constructor(private readonly salt: number) {}
 
   encrypt(value: string): string {
     return hashSync(value, this.salt);
+  }
+
+  compareEncrypt(compareValue: string, hashValue: string): boolean {
+    return compareSync(compareValue, hashValue);
   }
 }
 
