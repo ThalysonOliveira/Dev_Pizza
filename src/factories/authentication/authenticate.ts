c1e497a73a85c4387c86a8ebfbc7505d7f91aa73
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-24 22:25:26 -0300

    feat: create authentication factory

---
+++ src/factories/authentication/authenticate.ts
@@ -0,0 +1,18 @@
+import { AuthenticateController } from '../../controllers/autentication';
+import { AuthenticationService } from '../../data/services/authentication/authenticate';
+import { FindUserByEmailPrisma } from '../../infra/database/prisma/repositories/users/find-user-by-email-prisma';
+import { EncrypterAdapter } from '../../infra/protocols/encrypter/encrypter-adapter';
+import { TokenAdapter } from '../../infra/protocols/token/token-adapter';
+
+const makeAuthentication = () => {
+  const encrypter = new EncrypterAdapter(8);
+  const findUserbyEmail = new FindUserByEmailPrisma();
+  const token = new TokenAdapter();
+  const authenticationService = new AuthenticationService(
+    findUserbyEmail,
+    encrypter,
+    token
+  );
+  return new AuthenticateController(authenticationService);
+};
+export { makeAuthentication };
