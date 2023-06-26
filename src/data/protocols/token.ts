7a577a9319a15eb0d95c54ca3565db5cf38c7e7e
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-26 10:50:24 -0300

    feat: create interface and verifyToken implementation

--- src/data/protocols/token.ts
+++ src/data/protocols/token.ts
@@ -1,6 +1,14 @@
 /* eslint-disable no-unused-vars */
+
+type TokenPayload = {
+  id: string;
+  iat: number;
+  exp: number;
+};
+
 interface Token {
   createToken: (value: any) => string;
+  verifyToken: (token: string) => TokenPayload;
 }
 
-export { Token };
+export { Token, TokenPayload };
--- src/infra/protocols/token/token-adapter.ts
+++ src/infra/protocols/token/token-adapter.ts
@@ -1,11 +1,15 @@
-import { Token } from '../../../data/protocols';
-import { sign } from 'jsonwebtoken';
+import { Token, TokenPayload } from '../../../data/protocols';
+import { sign, verify } from 'jsonwebtoken';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 
 class TokenAdapter implements Token {
   createToken(value: any): string {
     return sign({ id: value.id }, JWT_SECRET, { expiresIn: '7d' });
+  }
+
+  verifyToken(token: string): TokenPayload {
+    return verify(token, JWT_SECRET) as TokenPayload;
   }
 }
 
