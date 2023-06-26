6470a5fda7118aed6a7c3d4ca8de083533dab3b4
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-26 14:12:42 -0300

    feat: create user detail factory

---
+++ src/factories/users/detail-user.ts
@@ -0,0 +1,11 @@
+import { DetailUserService } from '../../data/services/users';
+import { FindUserByIdPrisma } from '../../infra/database/prisma/repositories/users/find-user-by-id';
+import { DetailUserController } from '../../presentation/controllers/users/detail.user';
+
+const makeDetailUser = () => {
+  const findUserById = new FindUserByIdPrisma();
+  const detailUserService = new DetailUserService(findUserById);
+  return new DetailUserController(detailUserService);
+};
+
+export { makeDetailUser };
