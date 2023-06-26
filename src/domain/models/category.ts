af6758758f15acaf099ee1f8bed2dad83c7dd6f1
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-26 17:16:14 -0300

    feat: create model and use case category

---
+++ src/domain/models/category.ts
@@ -0,0 +1,8 @@
+type Category = {
+  id: string;
+  name: string;
+  created_at: Date;
+  updated_at: Date;
+};
+
+export { Category };
---
+++ src/domain/useCases/categories/create-category.ts
@@ -0,0 +1,9 @@
+type CategoryData = {
+  name: string;
+};
+
+interface CreateCategory {
+  execute: (input: CategoryData) => Promise<string>;
+}
+
+export { CreateCategory };
