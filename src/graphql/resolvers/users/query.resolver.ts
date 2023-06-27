69a6538edfc11a50eec18b09dab149e03656b6ef
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-27 12:12:35 -0300

    feat: add user id context validation

--- src/graphql/resolvers/users/query.resolver.ts
+++ src/graphql/resolvers/users/query.resolver.ts
@@ -1,9 +1,12 @@
 import { makeDetailUser } from '@factories/users';
 import { Context } from '@type/middlewares';
+import { userIdContextValidate } from '@utils/context-validate';
 
 export default {
   Query: {
-    detailUser: (_: never, _input: never, { userId }: Context) =>
-      makeDetailUser().handle(userId),
+    detailUser: (_: never, _input: never, { userId }: Context) => {
+      userIdContextValidate(userId);
+      return makeDetailUser().handle(userId);
+    },
   },
 };
