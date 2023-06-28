89cfd19f2251c4c3e0b6b2b69e719f4bbaeb6f14
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-28 13:35:40 -0300

    feat: change return

--- src/domain/useCases/products/list-all-products-by-category.ts
+++ src/domain/useCases/products/list-all-products-by-category.ts
@@ -1,7 +1,7 @@
 import { Product } from '@domain/models';
 
 interface ListAllProductsByCategory {
-  execute: (categoryId: string) => Promise<Product[]>;
+  execute: (categoryId: string) => Promise<Partial<Product>[]>;
 }
 
 export { ListAllProductsByCategory };
