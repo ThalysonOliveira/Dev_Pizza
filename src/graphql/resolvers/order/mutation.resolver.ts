e0fc6bf150e2f710ca61e58f0e8df7b4ee97fc29
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-06-29 15:59:26 -0300

    feat: create mutation and resolver for add item to order

--- src/graphql/resolvers/order/mutation.resolver.ts
+++ src/graphql/resolvers/order/mutation.resolver.ts
@@ -1,5 +1,9 @@
-import { OrderData } from '@domain/useCases/order';
-import { makeCreateOrder, makeRemoveOrder } from '@factories/order';
+import { ItemData, OrderData } from '@domain/useCases/order';
+import {
+  makeAddItemToOrder,
+  makeCreateOrder,
+  makeRemoveOrder,
+} from '@factories/order';
 import { InputType } from '@type/generics';
 import { Context } from '@type/middlewares';
 import { userIdContextValidate } from '@utils/context-validate';
@@ -21,6 +25,14 @@ export default {
     ) => {
       userIdContextValidate(userId);
       return makeRemoveOrder().handle(orderId);
+    },
+    addItemToOrder: (
+      _: never,
+      { input }: InputType<ItemData>,
+      { userId }: Context
+    ) => {
+      userIdContextValidate(userId);
+      return makeAddItemToOrder().handle(input);
     },
   },
 };
