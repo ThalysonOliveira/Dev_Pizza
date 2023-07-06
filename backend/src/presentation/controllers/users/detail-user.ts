ef0f637b5e0690a59c35169ed9ad9f391a255d8a
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-07-06 08:56:01 -0300

    refactor: remove unnecessary validation

--- backend/src/presentation/controllers/users/detail-user.ts
+++ backend/src/presentation/controllers/users/detail-user.ts
@@ -1,17 +1,22 @@
 import { User } from '@domain/models';
 import { DetailUser } from '@domain/useCases/users';
-import { invalidUserOrNotAuthorized } from '@presentation/errors';
+import {
+  getErrorResponse,
+  invalidUserOrNotAuthorized,
+} from '@presentation/errors';
 
 class DetailUserController {
   constructor(private detailUser: DetailUser) {}
 
   async handle(id: string): Promise<string | Partial<User>> {
-    if (!id) return invalidUserOrNotAuthorized();
+    try {
+      const userResult = await this.detailUser.execute(id);
+      if (!userResult) return invalidUserOrNotAuthorized();
 
-    const userResult = await this.detailUser.execute(id);
-    if (!userResult) return invalidUserOrNotAuthorized();
-
-    return userResult;
+      return userResult;
+    } catch (error) {
+      return getErrorResponse(error);
+    }
   }
 }
 
