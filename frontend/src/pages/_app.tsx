cf49d59264cbb427506eec19af7f8e032609dac4
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-08-03 11:14:14 -0300

    refactor: adjust apollo client config

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from "next/app";
 import GlobalStyles from "../../styles";
 import { ApolloProvider } from "@apollo/client";
-import { client } from "@/api/dev-pizza-api";
+import { setupAPIClient } from "@/api/dev-pizza-api";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
+    <ApolloProvider client={setupAPIClient()}>
       <AuthProvider>
         <GlobalStyles />
         <Component {...pageProps} />
