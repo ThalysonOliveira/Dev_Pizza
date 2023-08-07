eb02e434ef538ad342094a3d480ff353309e1a4a
Thalyson Oliveira <thalysonoliveira150@gmail.com>
2023-08-07 08:31:54 -0300

    feat: create upload image interface and implementation

---
+++ backend/src/infra/upload/index.ts
@@ -0,0 +1,1 @@
+export * from './upload-iamge-adapter';
---
+++ backend/src/infra/upload/upload-iamge-adapter.ts
@@ -0,0 +1,37 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { UploadImage } from '@data/protocols/upload';
+import { getErrorResponse } from '@presentation/errors';
+
+const SDK_REGION = process.env.SDK_REGION;
+const SDK_ACCESS_KEY_ID = process.env.SDK_ACCESS_KEY_ID;
+const SDK_SECRET_ACCESS_KEY_ID = process.env.SDK_SECRET_ACCESS_KEY_ID;
+const SDK_BUCKET_NAME = process.env.SDK_BUCKET_NAME;
+
+class UploadImageAdapter implements UploadImage {
+  private readonly client: S3Client;
+
+  constructor() {
+    this.client = new S3Client({
+      region: SDK_REGION,
+      credentials: {
+        accessKeyId: SDK_ACCESS_KEY_ID,
+        secretAccessKey: SDK_SECRET_ACCESS_KEY_ID,
+      },
+    });
+  }
+
+  async execute(image: any): Promise<void> {
+    try {
+      const command = new PutObjectCommand({
+        Bucket: SDK_BUCKET_NAME,
+        Key: image.filename,
+        Body: Buffer.from(JSON.stringify(image)),
+      });
+      await this.client.send(command);
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
+}
+
+export { UploadImageAdapter };
